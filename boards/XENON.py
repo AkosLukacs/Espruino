fe206723df8ab7536c29ecaed916edcd15a743d8
Lukács Ákos <akoslukacs42@gmail.com>
2024-07-14 18:14:30 +0200

    Nucleo 411 with net

--- boards/XENON.py
+++ boards/XENON.py
@@ -16,42 +16,46 @@
 import pinutils;
 
 info = {
- 'name' : "nRF52840 Development Kit",
- 'link' :  [ "https://www.nordicsemi.com/eng/Products/Bluetooth-low-energy/nRF52-DK" ],
+ 'name' : "Particle Xenon",
+ 'link' :  [ "https://docs.particle.io/datasheets/discontinued/xenon-datasheet/" ],
  'espruino_page_link' : 'nRF52840DK',
-  # This is the PCA10036
- 'default_console' : "EV_SERIAL1",
- 'default_console_tx' : "D6",
- 'default_console_rx' : "D8",
- 'default_console_baudrate' : "9600",
+ 'default_console' : "EV_BLUETOOTH",
  'variables' : 12500, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
- 'binary_name' : 'espruino_%v_nrf52840.hex',
+ 'binary_name' : 'espruino_%v_xenon.hex',
  'build' : {
    'optimizeflags' : '-Os',
    'libraries' : [
      'BLUETOOTH',
-     'NET',
+#     'TERMINAL',
      'GRAPHICS',
-#     'NFC',
-     'NEOPIXEL',
+     'CRYPTO','SHA256','SHA512',
      'JIT' # JIT compiler enabled
    ],
    'makefile' : [
      'DEFINES += -DCONFIG_GPIO_AS_PINRESET', # Allow the reset pin to work
-     'DEFINES += -DBOARD_PCA10056',
+     'DEFINES += -DCONFIG_NFCT_PINS_AS_GPIOS', # Allow us to use NFC pins as GPIO
      'DEFINES += -DNRF_USB=1 -DUSB',
-     'DEFINES += -DNEOPIXEL_SCK_PIN=22 -DNEOPIXEL_LRCK_PIN=23', # nRF52840 needs LRCK pin defined for neopixel
+     'DEFINES += -DESPR_LSE_ENABLE', # Ensure low speed external osc enabled
      'DEFINES += -DNRF_SDH_BLE_GATT_MAX_MTU_SIZE=131', # 23+x*27 rule as per https://devzone.nordicsemi.com/f/nordic-q-a/44825/ios-mtu-size-why-only-185-bytes
      'DEFINES += -DCENTRAL_LINK_COUNT=2 -DNRF_SDH_BLE_CENTRAL_LINK_COUNT=2', # allow two outgoing connections at once
      'LDFLAGS += -Xlinker --defsym=LD_APP_RAM_BASE=0x3660', # set RAM base to match MTU=131 + CENTRAL_LINK_COUNT=2
-#     'ESPR_BLUETOOTH_ANCS=1', # Enable ANCS (Apple notifications) support
-     'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0xa9,0xae,0xb6',
+     'DEFINES += -DESPR_DCDC_ENABLE=1', # Use DC/DC converter
+     'ESPR_BLUETOOTH_ANCS=1', # Enable ANCS (Apple notifications) support
+#    'DEFINES += -DSPIFLASH_SLEEP_CMD', # SPI flash needs to be explicitly slept and woken up
+#    'DEFINES += -DSPIFLASH_READ2X', # Read SPI flash at 2x speed using MISO and MOSI for IO
+     'DEFINES += -DESPR_JSVAR_FLASH_BUFFER_SIZE=32', # The buffer size we use when executing/iterating over data in flash memory (default 16). Should be set based on benchmarks.
+     'DEFINES+=-DUSE_FONT_6X8 -DGRAPHICS_PALETTED_IMAGES -DGRAPHICS_ANTIALIAS',
+     'DEFINES+=-DBLE_HIDS_ENABLED=1 -DBLUETOOTH_NAME_PREFIX=\'"XENON"\'',
+     'DEFINES+=-DLCD_HEIGHT=122 -DLCD_WIDTH=250',
+     'DEFINES+=-DDUMP_IGNORE_VARIABLES=\'"g\\0"\'',
+     'DEFINES+=-DNO_DUMP_HARDWARE_INITIALISATION', # don't dump hardware init - not used and saves 1k of flash
+     'DEFINES += -DESPR_NO_LINE_NUMBERS=1', # we execute mainly from flash, so line numbers can be worked out
      'DFU_PRIVATE_KEY=targets/nrf5x_dfu/dfu_private_key.pem',
-     'DEFINES += -DNRF_BOOTLOADER_NO_WRITE_PROTECT=1', # By default the bootloader protects flash. Avoid this (a patch for NRF_BOOTLOADER_NO_WRITE_PROTECT must be applied first)
-#     'DEFINES += -DBUTTONPRESS_TO_REBOOT_BOOTLOADER', # enables watchdog timer
+     'DFU_SETTINGS=--application-version 0xff --hw-version 52 --sd-req 0xa9', #S140 6.0.0
      'BOOTLOADER_SETTINGS_FAMILY=NRF52840',
-     'NRF_SDK15=1',
+     'DEFINES += -DNRF_BL_DFU_INSECURE=1',
+     'NRF_SDK15=1'
    ]
  }
 };
@@ -60,75 +64,53 @@ info = {
 chip = {
   'part' : "NRF52840",
   'family' : "NRF52",
-  'package' : "AQFN73",
+  'package' : "QFN48",
   'ram' : 256,
   'flash' : 1024,
   'speed' : 64,
   'usart' : 2,
-  'spi' : 3,
-  'i2c' : 2,
+  'spi' : 1,
+  'i2c' : 1,
   'adc' : 1,
   'dac' : 0,
   'saved_code' : {
-    'address' : ((246 - 10) * 4096), # Bootloader takes pages 248-255, FS takes 246-247
+    'address' : 0x60000000,
     'page_size' : 4096,
-    'pages' : 10,
-    'flash_available' : 1024 - ((31 + 8 + 2 + 10)*4) # Softdevice uses 31 pages of flash, bootloader 8, FS 2, code 10. Each page is 4 kb.
+    'pages' : 1024, # use all of flash
+    'flash_available' : 1024 - ((38 + 8 + 2)*4) # Softdevice uses 38 pages of flash (0x26000/0x100), bootloader 8, FS 2, code 20. Each page is 4 kb.
   },
 };
 
 devices = {
   'BTN1' : { 'pin' : 'D11', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
-  'BTN2' : { 'pin' : 'D12', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
-  'BTN3' : { 'pin' : 'D24', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
-  'BTN4' : { 'pin' : 'D25', 'pinstate' : 'IN_PULLDOWN' }, # Pin negated in software
+#RGB LED
   'LED1' : { 'pin' : 'D13' }, # Pin negated in software
   'LED2' : { 'pin' : 'D14' }, # Pin negated in software
   'LED3' : { 'pin' : 'D15' }, # Pin negated in software
-  'LED4' : { 'pin' : 'D16' }, # Pin negated in software
   'RX_PIN_NUMBER' : { 'pin' : 'D8'},
   'TX_PIN_NUMBER' : { 'pin' : 'D6'},
-  'CTS_PIN_NUMBER' : { 'pin' : 'D7'},
-  'RTS_PIN_NUMBER' : { 'pin' : 'D5'},
-  # Pin D22 is used for clock when driving neopixels - as not specifying a pin seems to break things
+#  'CTS_PIN_NUMBER' : { 'pin' : 'D7'},
+#  'RTS_PIN_NUMBER' : { 'pin' : 'D5'},
+  'SPIFLASH' : {
+            'pin_cs' : 'D17',
+            'pin_sck' : 'D19',
+            'pin_mosi' : 'D20',
+            'pin_miso' : 'D21',
+            'pin_wp' : 'D22',
+#            'pin_hold' : 'D23',
+            'pin_rst' : 'D23', # no reset but this is HOLD pin for XENON, we want it set to 1
+            'size' : 4096*1024, # 4MB
+            'memmap_base' : 0x60000000,
+          }
+
+
 };
 
 # left-right, or top-bottom order
+
 board = {
-  'left' : [ 'VDD', 'VDD', 'RESET', 'VDD','5V','GND','GND','','','D3','D4','D28','D29','D30','D31'],
-  'right' : [
-     'D27', 'D26', 'D2', 'GND', 'D25','D24','D23', 'D22','D20','D19','',
-     'D18','D17','D16','D15','D14','D13','D12','D11','',
-     'D10','D9','D8','D7','D6','D5','D21','D1','D0'],
-  '_notes' : {
-    'D6' : "Serial console RX",
-    'D8' : "Serial console TX"
-  }
 };
-board["_css"] = """
-#board {
-  width: 528px;
-  height: 800px;
-  top: 0px;
-  left : 200px;
-  background-image: url(img/NRF528DK.jpg);
-}
-#boardcontainer {
-  height: 900px;
-}
 
-#left {
-    top: 219px;
-    right: 466px;
-}
-#right {
-    top: 150px;
-    left: 466px;
-}
-
-.leftpin { height: 17px; }
-.rightpin { height: 17px; }
-""";
 
 def get_pins():
   pins = pinutils.generate_pins(0,47) # 48 General Purpose I/O Pins.
@@ -152,12 +134,7 @@ def get_pins():
   pinutils.findpin(pins, "PD13", True)["functions"]["NEGATED"]=0;
   pinutils.findpin(pins, "PD14", True)["functions"]["NEGATED"]=0;
   pinutils.findpin(pins, "PD15", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD16", True)["functions"]["NEGATED"]=0;
   pinutils.findpin(pins, "PD11", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD12", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD24", True)["functions"]["NEGATED"]=0;
-  pinutils.findpin(pins, "PD25", True)["functions"]["NEGATED"]=0;
-
   # everything is non-5v tolerant
   for pin in pins:
     pin["functions"]["3.3"]=0;
